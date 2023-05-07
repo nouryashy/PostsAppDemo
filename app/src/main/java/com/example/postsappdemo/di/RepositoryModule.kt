91447355f2b75e5fa5b97ee16699f92407464b5d
nouryashy <nourhanashmawy135@gmail.com>
2023-05-07 22:36:52 +0300

    create RepositoryModule.kt and add the functions in it

---
+++ app/src/main/java/com/example/postsappdemo/di/RepositoryModule.kt
@@ -0,0 +1,19 @@
+package com.example.postsappdemo.di
+
+import com.example.data.remote.ApiServices
+import com.example.data.repository.ArticleRepositoryImp
+import com.example.domain.repository.ArticleRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+    @Provides
+    fun provideRepository(apiServices: ApiServices): ArticleRepository {
+        return ArticleRepositoryImp(apiServices)
+    }
+
+}
