8bcb8c21cefe9cc452faf1cac898339e84f74b59
nouryashy <nourhanashmawy135@gmail.com>
2023-05-07 22:40:31 +0300

    create UseCaseModule.kt.kt and add the function in it

---
+++ app/src/main/java/com/example/postsappdemo/di/UseCaseModule.kt
@@ -0,0 +1,17 @@
+package com.example.postsappdemo.di
+
+import com.example.domain.repository.ArticleRepository
+import com.example.domain.usecase.getArticles
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object UseCaseModule {
+    @Provides
+    fun provideUseCase(articleRepository: ArticleRepository): getArticles {
+        return getArticles(articleRepository)
+    }
+}
