cf616aefda942299e53c739f10662a4e7bde5bd6
nouryashy <nourhanashmawy135@gmail.com>
2023-05-10 22:17:08 +0300

    update api link

--- app/src/main/java/com/example/postsappdemo/utils/Constants.kt
+++ app/src/main/java/com/example/postsappdemo/utils/Constants.kt
@@ -2,6 +2,6 @@ package com.example.postsappdemo.utils
 
 class Constants {
     companion object {
-        var BASE_URL = "https://newsapi.org/v2/top-headlines"
+        var BASE_URL = "https://newsapi.org/v2/"
     }
 }
